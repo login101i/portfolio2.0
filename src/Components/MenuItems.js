e0698712138d268c03f3c1c6b2e365a048e46c4a
McKrus <maciejkruszyniak@gmail.com>
2021-03-19 03:50:28 +0100

    github link/MenuItem

--- src/Components/MenuItems.js
+++ src/Components/MenuItems.js
@@ -10,13 +10,17 @@ function MenuItems({ portoflioss }) {
                             <img src={item.image} alt="" style={{ objectFit: 'contain' }} />
                             <ul className="hover-items">
                                 <li>
-                                    <a href={item.link1}> More </a>
+                                    <a href={item.link1} target="_blank"> More </a>
                                 </li>
                             </ul>
                         </div>
-                        <h5 style={{ textAlign: 'center' }}>
+                        <h5 style={{ textAlign: 'center', }}>
                             {item.title}
                         </h5>
+                        <div style={{ color: "#222" }} className="github" >
+                            <a href={item.link2} style={{ textAlign: 'center' }} target="_blank">Source code on github</a>
+                        </div>
+
 
                     </div>
                 })
