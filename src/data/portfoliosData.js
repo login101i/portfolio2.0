b27b61d2e846ae25d526b42fe1705963c20b5072
login101i <maciejkruszyniak@gmail.com>
2022-12-22 11:11:22 +0100

    styling update

--- src/data/portfoliosData.js
+++ src/data/portfoliosData.js
@@ -1,175 +1,175 @@
-import image1 from "../img/portImages/cukiernia_sowa.png";
-import image2 from "../img/portImages/dstreet.png";
-import image3 from "../img/portImages/movieDB.jpg";
-import image4 from "../img/portImages/furniture-shop.png";
-import image5 from "../img/portImages/travelly.jpg";
-import image6 from "../img/portImages/elena-joy.jpg";
-import image7 from "../img/portImages/akan.png";
-import image8 from "../img/portImages/7iphone-store.jpg";
-import image9 from "../img/portImages/netflix.jpg";
-import image10 from "../img/portImages/Rn_clothes_app.jpg";
-import image11 from "../img/portImages/BauleStore.png";
-import image12 from "../img/portImages/mern-project-podroze.png";
-import image13 from "../img/portImages/weather.png";
-import image14 from "../img/portImages/first_portfolio.png";
-import image15 from "../img/portImages/akansite.png";
-import image16 from "../img/portImages/allegroWeb.png";
-import image17 from "../img/portImages/booking_com.png";
+import image1 from '../img/portImages/cukiernia_sowa.png';
+import image2 from '../img/portImages/dstreet.png';
+import image3 from '../img/portImages/movieDB.jpg';
+import image4 from '../img/portImages/furniture-shop.png';
+import image5 from '../img/portImages/travelly.jpg';
+import image6 from '../img/portImages/elena-joy.jpg';
+import image7 from '../img/portImages/akan.png';
+import image8 from '../img/portImages/7iphone-store.jpg';
+import image9 from '../img/portImages/netflix.jpg';
+import image10 from '../img/portImages/Rn_clothes_app.jpg';
+import image11 from '../img/portImages/BauleStore.png';
+import image12 from '../img/portImages/mern-project-podroze.png';
+import image13 from '../img/portImages/weather.png';
+import image14 from '../img/portImages/first_portfolio.png';
+import image15 from '../img/portImages/akansite.png';
+import image16 from '../img/portImages/allegroWeb.png';
+import image17 from '../img/portImages/booking_com.png';
 
 const portfoliosData = [
 	{
 		id: 0,
-		category: "Rest API",
-		link1: "",
-		link2: "https://github.com/login101i/Booking.com_WEB",
+		category: 'Rest API',
+		link1: 'https://bookingcmkrus.herokuapp.com',
+		link2: 'https://github.com/login101i/Booking.com_WEB',
 		image: image17,
-		title: "Booking.com Clone ",
-		description: { header: "Opis w trakcie tworzenia.. ." }
+		title: 'Booking.com Clone ',
+		description: { header: 'Opis w trakcie tworzenia.. .' }
 	},
 	{
 		id: 1,
-		category: "E-commerce",
-		link1: "",
-		link2: "https://github.com/login101i/allegro_desktop",
+		category: 'E-commerce',
+		link1: 'https://allegroweb.herokuapp.com',
+		link2: 'https://github.com/login101i/allegro_desktop',
 		image: image16,
-		title: "www.allegro.pl Clone",
+		title: 'www.allegro.pl Clone',
 		description: null
 	},
 	{
 		id: 2,
-		category: "Wordpress",
-		link1: "http://www.akan.com.pl",
-		link2: "",
+		category: 'Wordpress',
+		link1: 'http://www.akan.com.pl',
+		link2: '',
 		image: image15,
-		title: "www.akan.com.pl",
+		title: 'www.akan.com.pl',
 		description: null
 	},
 
 	{
 		id: 3,
-		category: "E-commerce",
-		link1: "",
-		link2: "https://github.com/login101i/MERN-2-cukierniaSowa",
+		category: 'E-commerce',
+		link1: '',
+		link2: 'https://github.com/login101i/MERN-2-cukierniaSowa',
 		image: image1,
-		title: "Cukiernia Sowa Clone",
+		title: 'Cukiernia Sowa Clone',
 		description: null
 	},
 	{
 		id: 4,
-		category: "E-commerce",
-		link1: "",
-		link2: "https://github.com/login101i/MERN-1-dstreet",
+		category: 'E-commerce',
+		link1: '',
+		link2: 'https://github.com/login101i/MERN-1-dstreet',
 		image: image2,
-		title: "Dstreet, e-commerce site Clone",
+		title: 'Dstreet, e-commerce site Clone',
 		description: null
 	},
 	{
 		id: 5,
-		category: "Rest API",
-		link1: "https://moviedbapplication.netlify.app",
-		link2: "https://github.com/login101i/movie-database-app",
+		category: 'Rest API',
+		link1: 'https://moviedbapplication.netlify.app',
+		link2: 'https://github.com/login101i/movie-database-app',
 		image: image3,
-		title: "Movie DataBase",
+		title: 'Movie DataBase',
 		description: null
 	},
-	{
-		id: 6,
-		category: "Java Script",
-		link1: "",
-		link2: "https://github.com/login101i/react-beach-resort",
-		image: image4,
-		title: "Hotel Resort Project",
-		description: null
-	},
+	// {
+	// 	id: 6,
+	// 	category: 'Java Script',
+	// 	link1: '',
+	// 	link2: 'https://github.com/login101i/react-beach-resort',
+	// 	image: image4,
+	// 	title: 'Hotel Resort Project',
+	// 	description: null
+	// },
 
-	{
-		id: 7,
-		category: "HTML/CSS/JS",
-		link1: "",
-		link2: "",
-		image: image5,
-		title: "Travelly site",
-		description: null
-	},
-	{
-		id: 8,
-		category: "HTML/CSS/JS",
-		link1: "",
-		link2: "",
-		image: image6,
-		title: "Photographer site ",
-		description: null
-	},
+	// {
+	// 	id: 7,
+	// 	category: 'HTML/CSS/JS',
+	// 	link1: '',
+	// 	link2: '',
+	// 	image: image5,
+	// 	title: 'Travelly site',
+	// 	description: null
+	// },
+	// {
+	// 	id: 8,
+	// 	category: 'HTML/CSS/JS',
+	// 	link1: '',
+	// 	link2: '',
+	// 	image: image6,
+	// 	title: 'Photographer site ',
+	// 	description: null
+	// },
 	{
 		id: 9,
-		category: "Java Script",
-		link1: "",
-		link2: "https://github.com/login101i/akan-game",
+		category: 'Java Script',
+		link1: '',
+		link2: 'https://github.com/login101i/akan-game',
 		image: image7,
-		title: "Akan quiz",
-		description: null
-	},
-	{
-		id: 10,
-		category: "ReactJS",
-		link1: "",
-		link2: "https://github.com/login101i/Iphone-Store",
-		image: image8,
-		title: "Iphone store",
-		description: null
-	},
-	{
-		id: 11,
-		category: "Rest API",
-		link1: "",
-		link2: "https://github.com/login101i/netflix-clone",
-		image: image9,
-		title: "Netflix Clone",
-		description: null
-	},
-	{
-		id: 12,
-		category: "React Native",
-		link1: "",
-		link2: "https://github.com/login101i/RN-clothes-app",
-		image: image10,
-		title: "Clothes app with React Native",
-		description: null
-	},
-	{
-		id: 13,
-		category: "ReactJS",
-		link1: "",
-		link2: "https://github.com/login101i/Baule-store",
-		image: image11,
-		title: "Baule Store",
-		description: null
-	},
-	{
-		id: 14,
-		category: "MERN STACK",
-		link1: "",
-		link2: "https://github.com/login101i/Baule-store",
-		image: image12,
-		title: "Beautiful Places MERN PROJECT",
-		description: null
-	},
-	{
-		id: 15,
-		category: "Rest API",
-		link1: "",
-		link2: "https://github.com/login101i/weather-app-react",
-		image: image13,
-		title: "Weather app",
+		title: 'Akan quiz',
 		description: null
 	},
 	{
 		id: 16,
-		category: "HTML/CSS/JS",
-		link2: "",
-		link1: "https://github.com/login101i/portfolio",
+		category: 'HTML/CSS/JS',
+		link2: 'https://github.com/login101i/portfolio',
+		link1: 'https://portfolio-mc-krusold.netlify.app/projects/',
 		image: image14,
-		title: "First portfolio",
+		title: 'First portfolio',
+		description: null
+	},
+	{
+		id: 10,
+		category: 'ReactJS',
+		link1: '',
+		link2: 'https://github.com/login101i/Iphone-Store',
+		image: image8,
+		title: 'Iphone store',
+		description: null
+	},
+	{
+		id: 11,
+		category: 'Rest API',
+		link1: '',
+		link2: 'https://github.com/login101i/Netflix_backend_frontend',
+		image: image9,
+		title: 'Netflix Clone',
+		description: null
+	},
+	{
+		id: 12,
+		category: 'React Native',
+		link1: '',
+		link2: 'https://github.com/login101i/RN-clothes-app',
+		image: image10,
+		title: 'Clothes app with React Native',
+		description: null
+	},
+	{
+		id: 13,
+		category: 'ReactJS',
+		link1: '',
+		link2: 'https://github.com/login101i/Baule-store',
+		image: image11,
+		title: 'Baule Store',
+		description: null
+	},
+	{
+		id: 14,
+		category: 'MERN STACK',
+		link1: '',
+		link2: 'https://github.com/login101i/Baule-store',
+		image: image12,
+		title: 'Beautiful Places MERN PROJECT',
+		description: null
+	},
+	{
+		id: 15,
+		category: 'Rest API',
+		link1: '',
+		link2: 'https://github.com/login101i/weather-app-react',
+		image: image13,
+		title: 'Weather app',
 		description: null
 	}
 ];
